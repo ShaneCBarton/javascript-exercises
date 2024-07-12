5cbf95bb34bca6df5dd00a058bd89d8f6d752628
Shane Barton <shanecb@hotmail.ca>
2024-07-12 15:34:06 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
 
+    if (numOne > numTwo) {
+        let x = numOne;
+        numOne = numTwo;
+        numTwo = x;
+    }
+
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    for (let i = numOne; i <= numTwo; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
