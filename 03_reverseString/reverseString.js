2b901a6bb176cbaed46f35c984d2736b287f8534
Shane Barton <shanecb@hotmail.ca>
2024-07-12 14:08:05 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let chars = [];
+    for (let i = 0; i < stringToReverse.length; i++) {
+        chars.push(stringToReverse[i]);
+    }
 
+    let reversedString = chars.reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
